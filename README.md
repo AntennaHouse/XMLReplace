febad6a855f9ad5711194d103102cc476a3da74c
brugal <angelo@antennahouse.com>
2019-11-01 11:45:04 -0400

    --mask option to replace characters with 'T'.  Works with non-ascii characters as well.

--- README.md
+++ README.md
@@ -5,5 +5,8 @@ text nodes with a random character of the same class (upper, lower, numeral).
 
 Text within XML attributes is not modified.  Entities are also not modified.  Only ASCII characters are replaced.
 
+ * --mask option will replace all text node characters (except entities) with 'T'.  This option will also work for non-ascii characters.
+
+
 
 
--- src/XMLReplace.java
+++ src/XMLReplace.java
@@ -3,33 +3,52 @@ import java.util.*;
 
 public class XMLReplace {
 
-
-	
 	public static void main(String args[]){
-		
+
 		char[] caps = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 		char[] lower = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 		char[] numeric = {'1','2','3','4','5','6','7','8','9','0'};
 		Random rdm = new Random();
-		if(args.length < 2)
-		{
-			System.out.println("Usage: FOFile OutputFile");
-			return;
+		boolean mask = false;
+		String inputFileName = null;
+		String outputFileName = null;
+
+		for (int i = 0;  i < args.length;  i++) {
+		    if (args[i].equals("--mask")) {
+			mask = true;
+			continue;
+		    }
+
+		    if (inputFileName == null) {
+			inputFileName = args[i];
+			continue;
+		    } else if (outputFileName == null) {
+			outputFileName = args[i];
+			continue;
+		    } else {
+			System.out.println("unknown option: " + args[i]);
+			System.exit(1);
+		    }
 		}
-		
+		if(inputFileName == null  ||  outputFileName == null)
+		{
+			System.out.println("Usage: [--mask] FOFile OutputFile");
+			System.exit(1);
+		}
+
 		File fis;
 		File fos;
 		boolean replaceFlag = false;
 		try{
-			fis = new File(args[0]);
-			fos = new File(args[1]);
-			char[] v = new char[1]; 
+			fis = new File(inputFileName);
+			fos = new File(outputFileName);
+			char[] v = new char[1];
 			FileReader sr = new FileReader(fis);
 			FileWriter sw = new FileWriter(fos);
 			int z;
 			z=sr.read(v);
-			boolean ampFlag = false; 
-			
+			boolean ampFlag = false;
+
 			char temp;
 			while(z != -1)
 			{
@@ -47,6 +66,9 @@ public class XMLReplace {
 				else if(m == '\r' || m == '\n' || m == '\f' || m == '\"' || m == '\\' || m == '\'' || m == '\b' || m == '\t' || !Character.isLetterOrDigit(m))
 					m=m;
 				else if(replaceFlag == true && ampFlag == false){
+				    if (mask) {
+					m = 'T';
+				    } else {
 					if(Character.isDigit(m))
 						m = numeric[rdm.nextInt(10)];
 					else if(Character.isUpperCase(m))
@@ -58,10 +80,9 @@ public class XMLReplace {
 						m = lower[rdm.nextInt(26)];
 					else
 						m=m;
-					//m = 'T';
+				    }
 				}
-				
-					
+
 				sw.write(m);
 				z=sr.read(v);
 			}
@@ -70,8 +91,6 @@ public class XMLReplace {
 		}catch(Exception e){
 			System.out.println("Error " + e);
 		}
-		
 	}
-	
-	
+
 }
